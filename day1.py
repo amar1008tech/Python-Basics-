a16d3e3d4da71aca6510140debda9549cf121b39
amar1008tech <amarjojare2@gmail.com>
2026-02-01 20:50:06 +0530

    added python variables,datatype and operator code

---
+++ day1.py
@@ -0,0 +1,6 @@
+#https://github.com/amar1008tech/Python-Basics-.git
+#variables,datatype,operators
+a=10
+b=11.2
+c=a+b
+print("Addition=",c)#here a,b,c are variables inbuilt datatype used in python and +,= are operator's used to print addition using print() 
