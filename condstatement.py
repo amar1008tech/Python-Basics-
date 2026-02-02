a8ea09f9f7011bc30494013253f44541303a9cad
amar1008tech <amarjojare2@gmail.com>
2026-02-03 03:31:59 +0530

    Added loops and condinal statements

---
+++ condstatement.py
@@ -0,0 +1,35 @@
+#if else
+'''
+a=int(input("Enter Number:"))
+if(a%2==0):
+    print(a,"Even")
+else:
+    print(a,"Odd")
+'''
+#nested if else
+'''
+a=int(input("Enter Number"))
+b=int(input("Enter Number"))
+c=int(input("Enter Number"))
+if(a>b):
+    if(a>c):
+        print(a,"Graeror")
+    else:
+        print(c,"Greator")
+else:
+    if(b>c):
+        print(b,"Greator")
+    else:
+        print(c,"Greator")
+'''
+#else if
+n=int(input("Enetr Number"))
+if(n==1):
+    print("One")
+elif(n==2):
+    print("Two")
+elif(n==3):
+    print("Three")
+else:
+    print("Invalid Input")
+
---
+++ loops.py
@@ -0,0 +1,11 @@
+#for loop
+'''for i in range(1,10+1,1):#range(start,stop-1,step)
+    print(i)
+'''
+#while loop
+'''
+a=10
+while(a>0):#initialised a=10 condtion a>0 and decrement a=a-1
+    print(a)
+    a=a-1
+'''
